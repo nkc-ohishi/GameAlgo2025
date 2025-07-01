f98c96c42b8063adb147cf3225eab9f5e5f26d68
nkc23-ohishi <nkc23.kenji.ohishi@gmail.com>
2025-07-01 17:43:28 +0900

    アイテム追加途中

---
+++ Assets/Lesson/Lesson004/ItemController004.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class ItemController004 : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Rotate(1, 0, 0);
+    }
+}
